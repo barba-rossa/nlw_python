46c727199f79ffd02c5eede4979ec351dbdc43ae
barba-rossa <barbosaa.felipe@gmail.com>
2024-02-10 16:46:49 -0700

    feat: implement error handling

--- run_raw.py
+++ run_raw.py
@@ -4,16 +4,18 @@ from barcode.writer import ImageWriter
 
 app = Flask(__name__)
 
-@app.route('/create_tag', methods=['POST'])
+
+@app.route("/create_tag", methods=["POST"])
 def create_tag():
     body = request.json
-    product_code = body.get('product_code')
+    product_code = body.get("product_code")
 
     tag = Code128(product_code, writer=ImageWriter())
-    path_from_tag = f'{tag}'
+    path_from_tag = f"{tag}"
     tag.save(path_from_tag)
 
-    return jsonify({ "tag_path": path_from_tag })
+    return jsonify({"tag_path": path_from_tag})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,14 +1,19 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
+
+
 class TagCreatorView:
-    '''
-        responsability for interacting with HTTP
-    '''
+    """
+    responsability for interacting with HTTP
+    """
+
     def validate_and_create(self, http_request: HttpRequest):
+        tag_creator_controller = TagCreatorController()
         body = http_request.body
         product_code = body["product_code"]
 
-        #return business logic
-        print(f"Creating tag for product code {product_code}")
-        #return http response
-        return HttpResponse(200, {"success": "success"})
+        # return business logic
+        formatted_response = tag_creator_controller.create(product_code)
+        # return http response
+        return HttpResponse(status_code=200, body=formatted_response)
